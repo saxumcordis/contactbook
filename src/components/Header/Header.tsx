78c6f734add3d8f4b8345ef7e119b83db5c0c7cd
saxumcordis <victor123952@gmail.com>
2020-11-30 18:50:31 +0300

    add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './Header.module.scss';
+
+
+import {AddPersonButton} from "./components/AddPersonButton";
+import {SearchInput} from "./components/SearchInput";
+
+export const Header = () => {
+  return <nav className={styles.header}>
+    <AddPersonButton />
+    <SearchInput/>
+  </nav>;
+};
