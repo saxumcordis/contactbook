12ffd1499f75ebdc5b62c77d8e112e84f2f1e632
saxumcordis <victor123952@gmail.com>
2020-12-01 00:39:58 +0300

    add types + InputField component

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "antd/es/input/style/index.css";
+
+import { Input } from "antd";
+import { InputProps } from "antd/lib/input";
+
+export const InputField: React.FC<InputProps> = ({
+  className,
+  suffix,
+  prefix,
+  placeholder,
+}) => {
+  return (
+    <Input
+      className={className}
+      suffix={suffix}
+      prefix={prefix}
+      placeholder={placeholder}
+    />
+  );
+};
---
+++ src/types/Contact.ts
@@ -0,0 +1,32 @@
+export interface Contact_person_data_phone {
+    mobile: string | null;
+    work: string | null;
+    home: string | null;
+}
+
+export interface Contact_person_data_address {
+    country: string | null;
+    city: string | null;
+    street: string | null;
+    house: string | null;
+    flat: string | null;
+    postalCode: string | null;
+}
+
+export interface Contact_person_data {
+    phone: Contact_person_data_phone;
+    address: Contact_person_data_address;
+}
+
+export interface Contact_person {
+    _id: string;
+    name: string;
+    surname: string | null;
+    fatherName: string | null;
+    avatar: string;
+    data: Contact_person_data | null;
+}
+
+export interface Contact {
+    contact: Contact_person | null;
+}
