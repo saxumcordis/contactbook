eeee917eae2c83851c815d323347bd7116374c62
saxumcordis <victor123952@gmail.com>
2020-12-02 00:16:07 +0300

    rename avatar

---
+++ src/components/ContactAvatar/ContactAvatar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import styles from "./ContactAvatar.module.scss";
+
+import { Avatar } from "antd";
+
+import { UserOutlined } from "@ant-design/icons";
+
+type Props = {
+  avatar: string;
+};
+
+export const ContactAvatar: React.FC<Props> = ({ avatar }) => {
+  const defaultAvatar = <UserOutlined />;
+
+  return (
+    <Avatar
+      className={styles.contactAvatar}
+      icon={avatar === "default" && defaultAvatar}
+    />
+  );
+};
