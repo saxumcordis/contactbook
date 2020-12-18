2a6dcc8ac18c6196d68f463dc29956fca031df3a
saxumcordis <victor123952@gmail.com>
2020-12-19 00:33:20 +0300

    implement ContactModal component

--- src/ui/ContactsPlace/components/ContactBox/ContactBox.tsx
+++ src/ui/ContactsPlace/components/ContactBox/ContactBox.tsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ContactBox.module.scss";
 
 import { Contact_person } from "../../../../types/Contact";
-import { ContactAvatar } from "../../../../components/ContactAvatar";
-import { ContactBoxInfo } from "./components/ContactBoxInfo"
-
+import { SmallAvatar } from "../../../../components/SmallAvatar";
+import { ContactBoxInfo } from "./components/ContactBoxInfo";
+import { ContactModal } from "../ContactModal";
 
 type Props = {
   contact: Contact_person;
 };
 
 export const ContactBox: React.FC<Props> = ({ contact }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!isOpen);
+
   const { avatar } = contact;
   return (
-    <div className={styles.contactBox}>
-      <ContactAvatar avatar={avatar} />
+    <div className={styles.contactBox} onClick={() => !isOpen && handleOpen()}>
+      <SmallAvatar avatar={avatar} />
       <ContactBoxInfo contact={contact} />
+      <ContactModal contact={contact} isOpen={isOpen} setOpen={setOpen} />
     </div>
   );
 };
