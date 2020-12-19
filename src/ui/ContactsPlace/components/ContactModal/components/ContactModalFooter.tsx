7fa15323856a874696b24567632a7ebd7386d906
saxumcordis <victor123952@gmail.com>
2020-12-19 16:19:19 +0300

    update Footer to use Formik func

--- src/ui/ContactsPlace/components/ContactModal/components/ContactModalFooter.tsx
+++ src/ui/ContactsPlace/components/ContactModal/components/ContactModalFooter.tsx
@@ -1,14 +1,32 @@
-import React from "react";
-import {Contact_person} from "../../../../../types/Contact";
-import {Button} from "../../../../../components/Button";
+import React, { useCallback } from "react";
+import { Contact_person } from "../../../../../types/Contact";
+import { Button } from "../../../../../components/Button";
+
+import styles from './ContactModalFooter.module.scss';
 
 type Props = {
-    contact: Contact_person;
-}
+  contact: Contact_person;
+  dirty: boolean;
+  handleCancelButton: () => void;
+};
 
 //TODO Изменение темы кнопки по сохранению.
 
-export const ContactModalFooter: React.FC<Props> = ({contact}) => {
+export const ContactModalFooter: React.FC<Props> = ({
+  contact,
+  dirty,
+  handleCancelButton,
+}) => {
+  const handleCancel = useCallback(() => {
+    handleCancelButton();
+  }, [handleCancelButton]);
 
-    return <Button>Сохранить</Button>;
+  return (
+    <div className={styles.footer}>
+      <Button className={styles.button} htmlType="button" disabled={!dirty} onClick={handleCancel}>
+        Отменить
+      </Button>
+      <Button className={styles.button} htmlType="submit" disabled={!dirty} >Сохранить</Button>
+    </div>
+  );
 };
