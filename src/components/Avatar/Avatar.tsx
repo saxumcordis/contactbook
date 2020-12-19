49e706e92a84c7a125094df88bbfb1961b196ad5
saxumcordis <victor123952@gmail.com>
2020-12-19 16:16:25 +0300

    refactor SmallAvatar component to AvatarComponent

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import styles from "./Avatar.module.scss";
+
+import { Avatar as AntdAvatar } from "antd";
+
+import { UserOutlined } from "@ant-design/icons";
+import classNames from "classnames";
+
+type Props = {
+  avatar: string;
+  className?: string;
+  size: string;
+};
+
+export const Avatar: React.FC<Props> = ({ avatar, className, size }) => {
+  const defaultAvatar = <UserOutlined />;
+
+  return (
+    <AntdAvatar
+      className={classNames({ [styles.defaultAvatar]: avatar === 'default' }, className, styles[size + "Size"])}
+      icon={avatar === "default" && defaultAvatar}
+    />
+  );
+};
