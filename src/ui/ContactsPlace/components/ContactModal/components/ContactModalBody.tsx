acd1aec8db03916d51e160030bdeff2d52cd0fbd
saxumcordis <victor123952@gmail.com>
2020-12-19 16:19:51 +0300

    update Body to use Formik func + fill some contact info

--- src/ui/ContactsPlace/components/ContactModal/components/ContactModalBody.tsx
+++ src/ui/ContactsPlace/components/ContactModal/components/ContactModalBody.tsx
@@ -1,13 +1,43 @@
 import React from "react";
-import {Contact_person} from "../../../../../types/Contact";
+import { Field } from "formik";
 
-type Props = {
-    contact: Contact_person;
-}
+import styles from "./ContactModalBody.module.scss";
+import { FieldGroup } from "../../../../../components/FieldGroup";
+import classNames from "classnames";
+import {Avatar} from "../../../../../components/Avatar";
 
-export const ContactModalBody: React.FC<Props> = ({contact}) => {
-
-    const {name, surname} = contact;
-
-    return <>{name} {surname}</>;
+export const ContactModalBody = () => {
+  return (
+    <div className={styles.form}>
+      <FieldGroup
+        className={classNames(styles.headingGroup, styles.noPaddingSide)}
+      >
+        <FieldGroup
+          layout="column"
+          className={classNames(
+            styles.shortWidth,
+            styles.noPadding,
+            styles.inputStyle,
+            styles.noBorderBottom
+          )}
+        >
+          <Field id="name" name="name" placeholder="Имя" />
+          <Field id="surname" name="surname" placeholder="Фамилия" />
+          <Field id="fatherName" name="fatherName" placeholder="Отчество" />
+        </FieldGroup>
+          <Avatar avatar={"default"} size={"default"} className={styles.avatar}/>
+      </FieldGroup>
+      <FieldGroup
+        className={classNames(
+          styles.noPaddingSide,
+          styles.shortWidth,
+          styles.inputStyle
+        )}
+      >
+        <Field id="mobile" name="mobile" placeholder="Моб.телефон" />
+        <Field id="home" name="home" placeholder="Дом.телефон" />
+        <Field id="work" name="work" placeholder="Моб.телефон" />
+      </FieldGroup>
+    </div>
+  );
 };
