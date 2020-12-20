e06764a576aa76a0db53266e7f7e8e9b30761361
saxumcordis <victor123952@gmail.com>
2020-12-20 14:33:02 +0300

    add LabeledInput component

---
+++ src/components/LabeledInput/LabeledInput.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Field } from 'formik';
+
+import styles from './LabeledInput.module.scss';
+
+type Props = {
+    className?: string;
+    id: string;
+    labelStyle?: string;
+    name: string;
+    placeholder?: string;
+    labelText: string;
+}
+
+export const LabeledInput: React.FC<Props> = props => {
+
+    const {className, id, labelStyle, name, placeholder, labelText} = props;
+
+    return (
+        <div className={classNames(className, styles.inputWrapper)}>
+            <label className={labelStyle}>{labelText}</label>
+            <Field className={styles.input} id={id} name={name} placeholder={placeholder} />
+        </div>
+    )
+};
