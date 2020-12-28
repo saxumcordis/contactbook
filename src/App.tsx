e2eca86afcc7e259e7ffae1045f0dbaf158e3a8f
Viktor Fedoseev <victor123952@gmail.com>
2020-12-28 18:50:12 +0300

    start use newContactModal context

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Header } from "./ui/Header";
 import { ContactsPlace } from "./ui/ContactsPlace";
 import { ContactBookProvider, PersistContactBook } from "./service/contexts";
+import { NewContactModalProvider } from "./service/contexts/useNewContactModal";
+import { NewContactModal } from "./ui/NewContactModal";
 
 export default function App() {
   return (
     <div className="App">
       <ContactBookProvider>
-        <Header />
-        <ContactsPlace />
-        <PersistContactBook />
+        <NewContactModalProvider>
+          <Header />
+          <ContactsPlace />
+          <NewContactModal />
+          <PersistContactBook />
+        </NewContactModalProvider>
       </ContactBookProvider>
     </div>
   );
--- src/ui/Header/components/AddPersonButton/AddPersonButton.tsx
+++ src/ui/Header/components/AddPersonButton/AddPersonButton.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import styles from "./AddPersonButton.module.scss";
 
 import { PlusOutlined } from "@ant-design/icons";
+import { useNewContactModal } from "../../../../service/contexts/useNewContactModal";
 
 export const AddPersonButton = () => {
+  const { isOpen, open } = useNewContactModal();
+
   return (
-    <div className={styles.addPersonButton}>
+    <div className={styles.addPersonButton} onClick={() => !isOpen && open()}>
       <PlusOutlined />
     </div>
   );
