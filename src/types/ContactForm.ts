d2f152f8939689bcdcc98c2a09e5927e27f89385
Viktor Fedoseev <victor123952@gmail.com>
2020-12-28 18:47:29 +0300

    add ContactForm type

---
+++ src/types/ContactForm.ts
@@ -0,0 +1,17 @@
+export interface ContactFormValues {
+  name: string;
+  surname: string | null;
+  fatherName: string | null;
+  avatar: string;
+  birth: string | null;
+  group: string | null;
+  country: string | null;
+  city: string | null;
+  street: string | null;
+  house: string | null;
+  flat: string | null;
+  postalCode: string | null;
+  mobile: string | null;
+  work: string | null;
+  home: string | null;
+}
