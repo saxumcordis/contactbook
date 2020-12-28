69aa5a09dd1f6f537958de8fce9c798b8a92c978
Viktor Fedoseev <victor123952@gmail.com>
2020-12-28 18:49:02 +0300

    remove static contacts. start use context

--- src/ui/ContactsPlace/ContactsPlace.tsx
+++ src/ui/ContactsPlace/ContactsPlace.tsx
@@ -2,63 +2,18 @@ import React from "react";
 import styles from "./ContactsPlace.module.scss";
 import { ContactsPlaceHeader } from "./components/ContactsPlaceHeader";
 import { ContactBox } from "./components/ContactBox";
+import { useContactBook } from "../../service/contexts";
 
 export const ContactsPlace = () => {
-  const contact = {
-    _id: "1",
-    name: "Alexander",
-    avatar: "default",
-    surname: "Alexandrov",
-    fatherName: "Alexandrovich",
-    birth: "12.12.2000",
-    group: "Семья",
-    data: {
-      phone: {
-        mobile: "89999999999",
-        work: "89999999999",
-        home: "999999",
-      },
-      address: {
-        country: "Россия",
-        city: "Москва",
-        street: "Вавилова",
-        house: "19",
-        flat: "10",
-        postalCode: "117312",
-      },
-    },
-  };
-
-  const phone = {
-    mobile: "1231",
-    work: null,
-    home: null,
-  };
-
-  const data = {
-    phone: phone,
-    address: null,
-  };
+  const { contactBook } = useContactBook();
 
   return (
     <div className={styles.contactsPlace}>
       <ContactsPlaceHeader />
       <div className={styles.contactsWrapper}>
-        <ContactBox contact={{ ...contact, surname: null }} />
-        <ContactBox
-          contact={{
-            ...contact,
-            data: { ...data, phone: { ...phone, mobile: null } },
-          }}
-        />
-        <ContactBox
-          contact={{ ...contact, surname: null, fatherName: null, data: null }}
-        />
-        <ContactBox contact={contact} />
-        <ContactBox contact={contact} />
-        <ContactBox contact={contact} />
-        <ContactBox contact={contact} />
-        <ContactBox contact={contact} />
+        {contactBook?.map((contact, index) => (
+          <ContactBox key={index} contact={contact} />
+        ))}
       </div>
     </div>
   );
