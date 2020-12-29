b0ef39be00c54e967ab2d6453e0a29c36ca6b1f1
Viktor Fedoseev <victor123952@gmail.com>
2020-12-29 23:38:30 +0300

    implement update contact func

--- src/ui/ContactsPlace/components/ContactModal/ContactModal.tsx
+++ src/ui/ContactsPlace/components/ContactModal/ContactModal.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useCallback } from "react";
 import { Contact } from "../../../../types/Contact";
 import { BaseModal } from "../../../../components/BaseModal";
 import { BaseModalProps } from "../../../../types/Components";
@@ -8,14 +8,17 @@ import { ContactModalFooter, ContactModalHeader } from "./components";
 import { ContactModalBody } from "./components";
 import { Form, Formik } from "formik";
 import { ContactFormValues } from "../../../../types/ContactForm";
+import { useContactBook } from "../../../../service/contexts";
 
 type Props = Contact & BaseModalProps;
 
 export const ContactModal: React.FC<Props> = (props) => {
   const { isOpen, setOpen, contact } = props;
 
-  const initialValues = useMemo<ContactFormValues>(() => {
-    return {
+  const { updateContact } = useContactBook();
+
+  const initialValues = useCallback(
+    () => ({
       name: contact?.name,
       surname: contact?.surname || "",
       fatherName: contact?.fatherName || "",
@@ -31,13 +34,46 @@ export const ContactModal: React.FC<Props> = (props) => {
       mobile: contact?.data?.phone?.mobile || "",
       work: contact?.data?.phone?.work || "",
       home: contact?.data?.phone?.home || "",
+    }),
+    [contact]
+  );
+
+  const handleUpdate = (values: ContactFormValues) => {
+    const updatedContact = {
+      _id: contact._id,
+      name: values?.name,
+      surname: values?.surname,
+      fatherName: values?.fatherName,
+      birth: values?.birth,
+      avatar: values?.avatar,
+      group: values?.group,
+      data: {
+        phone: {
+          mobile: values.mobile,
+          work: values.work,
+          home: values.home,
+        },
+        address: {
+          country: values.country,
+          city: values.city,
+          street: values.street,
+          house: values.house,
+          flat: values.flat,
+          postalCode: values.postalCode,
+        },
+      },
     };
-  }, [contact]);
+
+    updateContact?.(updatedContact);
+  };
 
   return (
     <Formik
-      initialValues={initialValues}
-      onSubmit={() => console.log(contact)}
+      enableReinitialize
+      initialValues={initialValues()}
+      onSubmit={(values) => {
+        handleUpdate(values);
+      }}
       render={({ dirty, resetForm }) => (
         <Form className={styles.form}>
           <BaseModal
