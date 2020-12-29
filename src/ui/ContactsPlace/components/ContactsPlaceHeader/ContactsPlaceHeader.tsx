326fc0138d493be7ba380a242ca05d402343992f
Viktor Fedoseev <victor123952@gmail.com>
2020-12-29 13:01:12 +0300

    add contact counter

--- src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
+++ src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./ContactsPlaceHeader.module.scss";
+import { useContactBook } from "../../../../service/contexts";
 
 export const ContactsPlaceHeader = () => {
+  const { length } = useContactBook();
   return (
     <div className={styles.contactsPlaceHeader}>
       <span>Все контакты</span>
       <span>Показать группу</span>
       <span>Сортировка</span>
-      <span>Всего контактов 0/243</span>
+      <span>Всего контактов {length}</span>
     </div>
   );
 };
