1e0abe7abc6e5e0e8c188e3e26e346d90d5ac53c
Viktor Fedoseev <victor123952@gmail.com>
2020-12-31 15:02:31 +0300

    add string handling service

---
+++ src/service/stringHandlers/stringHandlers.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const handleLongString = (str: string | null) => {
+  if (str && str.length > 12) return str[0] + str.slice(1, 9) + "...";
+  return str;
+};
+
+export const handleLongStringWithTip = (str: string | null) => {
+  if (str && str.length > 12)
+    return <span title={str}>{str[0] + str.slice(1, 9) + "..."}</span>;
+  return str;
+};
