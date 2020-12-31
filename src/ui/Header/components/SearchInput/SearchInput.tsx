1773a19e26e01c0df33d8ebacdee19e84c18d86e
Viktor Fedoseev <victor123952@gmail.com>
2020-12-31 15:04:31 +0300

    implement setting searchvalue to searchinput

--- src/ui/Header/components/SearchInput/SearchInput.tsx
+++ src/ui/Header/components/SearchInput/SearchInput.tsx
@@ -3,9 +3,17 @@ import styles from "./SearchInput.module.scss";
 
 import { SearchOutlined } from "@ant-design/icons";
 import { InputField } from "../../../../components/InputField";
+import { useContactBook } from "../../../../service/contexts";
 
 export const SearchInput = () => {
   const suffix = <SearchOutlined />;
+
+  const { setSearchValue } = useContactBook();
+
+  const handleSearchValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) setSearchValue?.(e.target.value.split(" "));
+    else setSearchValue?.([]);
+  };
 
   return (
     <div className={styles.searchInputWrapper}>
@@ -13,6 +21,7 @@ export const SearchInput = () => {
         className={styles.searchInput}
         suffix={suffix}
         placeholder="Start input contact name..."
+        onChange={handleSearchValue}
       />
     </div>
   );
