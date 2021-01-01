849b7dc83676a97223e220e04814d10cc1afa530
Viktor Fedoseev <victor123952@gmail.com>
2021-01-01 20:51:26 +0300

    implement searching contacts

--- src/ui/ContactsPlace/ContactsPlace.tsx
+++ src/ui/ContactsPlace/ContactsPlace.tsx
@@ -3,15 +3,34 @@ import styles from "./ContactsPlace.module.scss";
 import { ContactsPlaceHeader } from "./components/ContactsPlaceHeader";
 import { ContactBox } from "./components/ContactBox";
 import { useContactBook } from "../../service/contexts";
+import { isHavingSubstring } from "../../service/stringHandlers";
 
 export const ContactsPlace = () => {
-  const { contactBook } = useContactBook();
+  const { contactBook, searchValue } = useContactBook();
+
+  const searchWords = searchValue?.filter((e) => e !== "");
+
+  const bookForSearch =
+    (searchWords?.length &&
+      contactBook?.filter((contact) =>
+        searchWords?.reduce(
+          (a, b) =>
+            a *
+            (isHavingSubstring(contact.name, b) ||
+            isHavingSubstring(contact?.surname || "", b) ||
+            isHavingSubstring(contact?.data?.phone?.mobile || "", b)
+              ? 1
+              : 0),
+          1
+        )
+      )) ||
+    contactBook;
 
   return (
     <div className={styles.contactsPlace}>
       <ContactsPlaceHeader />
       <div className={styles.contactsWrapper}>
-        {contactBook?.map((contact, index) => (
+        {bookForSearch?.map((contact, index) => (
           <ContactBox key={index} contact={contact} />
         ))}
       </div>
