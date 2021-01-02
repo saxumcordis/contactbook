eed0f172bfc2f68e780fa09b57594db1f3546ec5
Viktor Fedoseev <victor123952@gmail.com>
2021-01-03 01:55:21 +0300

    prettier fix

--- src/assets/defaultAvatars/defaultAvatars.tsx
+++ src/assets/defaultAvatars/defaultAvatars.tsx
@@ -56,7 +56,7 @@ export const defaultAvatars = (avatarName: string, fill?: string) => {
     unicornAvatar: {
       name: "unicornAvatar",
       icon: <UnicornAvatar fill={fill} />,
-    }
+    },
   };
 
   return avatarList[avatarName];
