9994ea3b6f58ca9d8acb66eafd575a6d7b6a1cec
Viktor Fedoseev <victor123952@gmail.com>
2021-01-03 00:53:46 +0300

    fix

--- src/components/AvatarSelectorModal/components/AvatarSelectorModalFooter.tsx
+++ src/components/AvatarSelectorModal/components/AvatarSelectorModalFooter.tsx
@@ -4,25 +4,25 @@ import { Button } from "../../Button";
 import styles from "./AvatarSelectorModalFooter.module.scss";
 
 type Props = {
-    onSubmit: () => void;
-    handleCancelButton: () => void;
+  onSubmit: () => void;
+  handleCancelButton: () => void;
 };
 
 export const AvatarSelectorModalFooter: React.FC<Props> = (props) => {
-    const { handleCancelButton, onSubmit } = props;
+  const { handleCancelButton, onSubmit } = props;
 
-    const handleCancel = useCallback(() => {
-        handleCancelButton();
-    }, [handleCancelButton]);
+  const handleCancel = useCallback(() => {
+    handleCancelButton();
+  }, [handleCancelButton]);
 
-    const handleSubmit = useCallback(() => {
-        onSubmit();
-    }, [onSubmit]);
+  const handleSubmit = useCallback(() => {
+    onSubmit();
+  }, [onSubmit]);
 
-    return (
-        <div>
-            <Button onClick={handleCancel}>Отменить</Button>
-            <Button onClick={handleSubmit}>Выбрать</Button>
-        </div>
-    );
+  return (
+    <div className={styles.avatarSelectorFooter}>
+      <Button onClick={handleCancel}>Отменить</Button>
+      <Button onClick={handleSubmit}>Выбрать</Button>
+    </div>
+  );
 };
