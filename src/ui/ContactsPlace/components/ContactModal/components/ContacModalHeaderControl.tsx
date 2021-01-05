285f1dd3903b9882ded2084b862f1e37f75d7789
Viktor Fedoseev <victor123952@gmail.com>
2021-01-05 21:01:49 +0300

    update contactModal Control

--- src/ui/ContactsPlace/components/ContactModal/components/ContacModalHeaderControl.tsx
+++ src/ui/ContactsPlace/components/ContactModal/components/ContacModalHeaderControl.tsx
@@ -2,29 +2,51 @@ import React from "react";
 
 import { ReactComponent as DeleteIcon } from "./assets/delete.svg";
 import { ReactComponent as FavoriteIcon } from "./assets/favorite.svg";
+import { ReactComponent as FilledFavoriteIcon } from "./assets/filledFavorite.svg";
 
 import styles from "./ContactModalHeader.module.scss";
 import { useContactBook } from "../../../../../service/contexts";
 import { PopConfirm } from "../../../../../components/PopConfirm";
+import { Contact_person } from "../../../../../types/Contact";
+import { useGroups } from "../../../../../service/contexts/useGroups";
 
 type Props = {
-  id: number;
+  contact: Contact_person;
   setOpen: (value: boolean) => void;
 };
 
 export const ContactModalHeaderControl: React.FC<Props> = (props) => {
-  const { id, setOpen } = props;
+  const { contact, setOpen } = props;
 
-  const { removeContact } = useContactBook();
+  const { removeContact, updateContact } = useContactBook();
+
+  const { isInGroup, handleContactGroups } = useGroups();
 
   const handleRemove = () => {
-    removeContact?.(id);
+    removeContact?.(contact._id);
     setOpen(false);
+  };
+
+  const handleFavGroup = () => {
+    const groups = handleContactGroups?.(contact.groups || "", "Избранное");
+    updateContact?.({ ...contact, groups: groups || null });
   };
 
   return (
     <div className={styles.control}>
-      <FavoriteIcon fill={"#feacef"} />
+      {isInGroup?.(contact.groups || "", "Избранное") ? (
+        <FilledFavoriteIcon
+          onClick={() => handleFavGroup()}
+          fill={"#ff0011"}
+          title="Убрать из избранного"
+        />
+      ) : (
+        <FavoriteIcon
+          onClick={() => handleFavGroup()}
+          fill={"#feacef"}
+          title="Добавить в избранное"
+        />
+      )}
       <PopConfirm
         title="Удалить контакт?"
         okText="Да"
