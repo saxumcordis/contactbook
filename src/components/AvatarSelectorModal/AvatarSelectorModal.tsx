3924e5bd72a631d664b7630787f52dfd37fad869
Viktor Fedoseev <victor123952@gmail.com>
2021-01-05 16:14:04 +0300

    switch props

--- src/components/AvatarSelectorModal/AvatarSelectorModal.tsx
+++ src/components/AvatarSelectorModal/AvatarSelectorModal.tsx
@@ -7,16 +7,9 @@ import {
   AvatarSelectorModalFooter,
 } from "./components";
 import { Formik, Form } from "formik";
+import { AvatarSelectorProps } from "../../types/Components";
 
-type Props = {
-  avatar: string;
-  setAvatar: (value: string) => void;
-  isOpen: boolean;
-  setOpen: (value: boolean) => void;
-  changeAvatar: (avatar: string) => void;
-};
-
-export const AvatarSelectorModal: React.FC<Props> = (props) => {
+export const AvatarSelectorModal: React.FC<AvatarSelectorProps> = (props) => {
   const { avatar, setAvatar, isOpen, setOpen, changeAvatar } = props;
 
   const initialValues = { activeAvatar: avatar };
