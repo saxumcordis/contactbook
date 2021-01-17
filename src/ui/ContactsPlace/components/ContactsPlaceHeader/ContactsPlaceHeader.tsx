cae5d9cd0d69e67d453a2df6a41d7348c2841d2a
Viktor Fedoseev <victor123952@gmail.com>
2021-01-18 00:07:43 +0300

    add handle activeGroups

--- src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
+++ src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./ContactsPlaceHeader.module.scss";
 import { useContactBook } from "../../../../service/contexts";
+import { useGroups } from "../../../../service/contexts/useGroups";
+import { PopOver } from "../../../../components/PopOver";
+
+import { CheckOutlined } from "@ant-design/icons";
+import classNames from "classnames";
 
 type Props = {
   currentLength: number | undefined;
@@ -10,10 +15,48 @@ export const ContactsPlaceHeader: React.FC<Props> = (props) => {
   const { currentLength } = props;
   const { length, searchValue } = useContactBook();
 
+  const {
+    groups,
+    isGroupActive,
+    handleActiveGroup,
+    setActiveGroups,
+  } = useGroups();
+
+  const groupsList = (
+    <div className={styles.listWrapper}>
+      <ul className={styles.list}>
+        {groups?.map((group, i) => (
+          <li
+            className={styles.listItem}
+            key={i}
+            onClick={() => handleActiveGroup?.(group?.name)}
+          >
+            {group.name}
+            {isGroupActive?.(group.name) && <CheckOutlined />}
+          </li>
+        ))}
+      </ul>
+      <li
+        className={classNames(styles.listItem, styles.fixedListItem)}
+        onClick={() => setActiveGroups?.([])}
+      >
+        Показать все группы
+      </li>
+    </div>
+  );
+
   return (
     <div className={styles.contactsPlaceHeader}>
       <span>Все контакты</span>
-      <span>Показать группу</span>
+      <PopOver
+        content={groupsList}
+        placement="bottom"
+        trigger="click"
+        popOverClassName={styles.popOver}
+        objectClassName={styles.popOverObject}
+      >
+        <span>Показать группу</span>
+      </PopOver>
       <span>Сортировка</span>
       <span className={styles.fixedSpan}>
         {" "}
