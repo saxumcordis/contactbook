74f5cb2293bb23f8ae217295607fd5a872017d0c
Viktor Fedoseev <victor123952@gmail.com>
2021-01-27 00:38:52 +0300

    close contactModal when open GroupsModal

--- src/ui/ContactsPlace/components/ContactBox/ContactBox.tsx
+++ src/ui/ContactsPlace/components/ContactBox/ContactBox.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback, useState } from "react";
+import React, {useCallback, useState} from "react";
 import styles from "./ContactBox.module.scss";
 
 import { Contact_person } from "../../../../types/Contact";
 import { Avatar } from "../../../../components/Avatar";
 import { ContactBoxInfo } from "./ContactBoxInfo";
 import { ContactModal } from "../../../ContactModal";
+import {useGroupsModal} from "../../../../service/contexts/useGroupsModal";
 
 type Props = {
   contact: Contact_person;
@@ -13,6 +14,8 @@ type Props = {
 export const ContactBox: React.FC<Props> = ({ contact }) => {
   const [isOpen, setOpen] = useState(false);
 
+  const groupsModalValue = useGroupsModal();
+
   const handleOpen = useCallback(() => setOpen(true), [setOpen]);
 
   const { avatar } = contact;
@@ -20,7 +23,7 @@ export const ContactBox: React.FC<Props> = ({ contact }) => {
     <div className={styles.contactBox} onClick={() => !isOpen && handleOpen()}>
       <Avatar avatar={avatar} size={"small"} className={styles.avatar} />
       <ContactBoxInfo contact={contact} />
-      <ContactModal contact={contact} isOpen={isOpen} setOpen={setOpen} />
+      <ContactModal contact={contact} isOpen={isOpen && !groupsModalValue.isOpen} setOpen={setOpen} />
     </div>
   );
 };
