8a0f06e64cf0b7579e65c7c6a565ac35ab2c76e0
Viktor Fedoseev <victor123952@gmail.com>
2021-01-26 21:51:16 +0300

    implement groupsModal context. remove list, prepare for using modal

--- src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
+++ src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import styles from "./ContactsPlaceHeader.module.scss";
 import { useContactBook } from "../../../../service/contexts";
-import { useGroups } from "../../../../service/contexts/useGroups";
-import { PopOver } from "../../../../components/PopOver";
-
-import { CheckOutlined } from "@ant-design/icons";
-import classNames from "classnames";
+import { useGroupsModal } from "../../../../service/contexts/useGroupsModal";
 
 type Props = {
   currentLength: number | undefined;
@@ -14,49 +10,12 @@ type Props = {
 export const ContactsPlaceHeader: React.FC<Props> = (props) => {
   const { currentLength } = props;
   const { length } = useContactBook();
-
-  const {
-    groups,
-    isGroupActive,
-    handleActiveGroup,
-    setActiveGroups,
-  } = useGroups();
-
-  const groupsList = (
-    <div className={styles.listWrapper}>
-      <ul className={styles.list}>
-        {groups?.map((group, i) => (
-          <li
-            className={styles.listItem}
-            key={i}
-            onClick={() => handleActiveGroup?.(group?.name)}
-          >
-            {group.name}
-            {isGroupActive?.(group.name) && <CheckOutlined />}
-          </li>
-        ))}
-      </ul>
-      <li
-        className={classNames(styles.listItem, styles.fixedListItem)}
-        onClick={() => setActiveGroups?.([])}
-      >
-        Показать все группы
-      </li>
-    </div>
-  );
+  const { open } = useGroupsModal();
 
   return (
     <div className={styles.contactsPlaceHeader}>
       <span>Все контакты</span>
-      <PopOver
-        content={groupsList}
-        placement="bottom"
-        trigger="click"
-        popOverClassName={styles.popOver}
-        objectClassName={styles.popOverObject}
-      >
-        <span>Показать группу</span>
-      </PopOver>
+      <span onClick={open}>Управление группами</span>
       <span>Сортировка</span>
       <span className={styles.fixedSpan}>
         {currentLength === length
