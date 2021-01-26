6c400573505f5a43572636a0eed5073c99baf66d
Viktor Fedoseev <victor123952@gmail.com>
2021-01-26 23:41:28 +0300

    test

--- src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
+++ src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./ContactsPlaceHeader.module.scss";
 import { useContactBook } from "../../../../service/contexts";
 import { useGroupsModal } from "../../../../service/contexts/useGroupsModal";
+import {useMedia} from "use-media";
+import {handleLongString} from "../../../../service/stringHandlers";
 
 type Props = {
   currentLength: number | undefined;
@@ -12,6 +14,8 @@ export const ContactsPlaceHeader: React.FC<Props> = (props) => {
   const { length } = useContactBook();
   const { open } = useGroupsModal();
 
+  const isSmall = useMedia({ maxWidth: "450px" });
+
   return (
     <div className={styles.contactsPlaceHeader}>
       <span>Все контакты</span>
@@ -19,8 +23,8 @@ export const ContactsPlaceHeader: React.FC<Props> = (props) => {
       <span>Сортировка</span>
       <span className={styles.fixedSpan}>
         {currentLength === length
-          ? `Количество контактов ${length}`
-          : `Найдено контактов ${currentLength}`}
+          ? `Количество контактов ${isSmall ? handleLongString(length!.toString(), 0) : length}`
+          : `Найдено контактов ${isSmall ? handleLongString(currentLength!.toString(), 0) : currentLength}`}
       </span>
     </div>
   );
