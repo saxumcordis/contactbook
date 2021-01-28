8aea44339f32a016b7630ed7e4de77adc41d9a6e
Viktor Fedoseev <victor123952@gmail.com>
2021-01-29 00:57:42 +0300

    add types to args

--- src/service/stringHandlers/stringHandlers.tsx
+++ src/service/stringHandlers/stringHandlers.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-export const handleLongString = (str: string | null, limit: number) => {
+export const handleLongString = (
+  str: string | null | undefined,
+  limit: number
+) => {
   if (str && str.length > limit) return str.slice(0, limit - 3) + "...";
   return str;
 };
 
-export const handleLongStringWithTip = (str: string | null, limit: number) => {
+export const handleLongStringWithTip = (
+  str: string | null | undefined,
+  limit: number
+) => {
   if (str && str.length > limit)
     return <span title={str}>{str.slice(0, limit - 1) + "..."}</span>;
   return str;
