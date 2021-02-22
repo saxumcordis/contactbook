1bb3574389550a035d66bbc94604b6205e800ac1
Viktor Fedoseev <victor123952@gmail.com>
2021-02-22 12:45:36 +0300

    add showAllContact button

--- src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
+++ src/ui/ContactsPlace/components/ContactsPlaceHeader/ContactsPlaceHeader.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import styles from "./ContactsPlaceHeader.module.scss";
+import React, { useCallback } from "react";
 import { useContactBook } from "../../../../service/contexts";
 import { useGroupsModal } from "../../../../service/contexts/useGroupsModal";
 import { useMedia } from "use-media";
 import { handleLongStringWithTip } from "../../../../service/stringHandlers";
 import { useGroups } from "../../../../service/contexts/useGroups";
+import classNames from "classnames";
+
+import { EyeOutlined } from "@ant-design/icons";
+
+import styles from "./ContactsPlaceHeader.module.scss";
 
 type Props = {
   currentLength: number | undefined;
@@ -12,21 +16,34 @@ type Props = {
 
 export const ContactsPlaceHeader: React.FC<Props> = (props) => {
   const { currentLength } = props;
-  const { length, searchValue } = useContactBook();
+  const { length, searchValue, setSearchValue } = useContactBook();
   const { open } = useGroupsModal();
-  const { activeGroups } = useGroups();
+  const { activeGroups, setActiveGroups } = useGroups();
 
   const isSmall = useMedia({ maxWidth: "450px" });
 
+  const isContactBookNotFiltered =
+    currentLength === length && !activeGroups?.length && !searchValue;
+
+  const clearContactBookFilters = useCallback(() => {
+    setSearchValue?.("");
+    setActiveGroups?.([]);
+  }, [setSearchValue, setActiveGroups]);
+
   return (
     <div className={styles.contactsPlaceHeader}>
-      <span>Все контакты</span>
+      <EyeOutlined
+        className={classNames(styles.showAllContactsButton, {
+          [styles.showAllContactsButton_notActive]: isContactBookNotFiltered,
+        })}
+        onClick={() => !isContactBookNotFiltered && clearContactBookFilters()}
+      />
       <span className={styles.pointer} onClick={open}>
         Управление группами
       </span>
       <span>Сортировка</span>
       <span className={styles.fixedSpan}>
-        {currentLength === length && !activeGroups?.length && !searchValue
+        {isContactBookNotFiltered
           ? `${
               isSmall
                 ? "Кол-во контактов " +
