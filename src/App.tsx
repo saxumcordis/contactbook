6065d4bad8ccb586dbeb0b824474a77b3f4205a5
Viktor Fedoseev <victor123952@gmail.com>
2021-03-01 23:36:31 +0300

    implement user and auth contexts. add routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ContactsPlace } from "./ui/ContactsPlace";
 import { ContactBookProvider, PersistContactBook } from "./service/contexts";
 import { NewContactModalProvider } from "./service/contexts/useNewContactModal";
 import { NewContactModal } from "./ui/NewContactModal";
+import { AuthPage } from "./ui/Authorization";
 import {
   GroupsContextProvider,
   PersistGroups,
@@ -11,25 +12,47 @@ import {
 import { FavoritesButton } from "./ui/FavoritesButton";
 import { GroupsModalContextProvider } from "./service/contexts/useGroupsModal";
 import { GroupsModal } from "./ui/GroupsModal";
+import { AuthRoute } from "./components/AuthRoute";
+import { Redirect, BrowserRouter, Switch, Route } from "react-router-dom";
+import { useUser } from "./service/contexts/useUser";
+import { AuthDataProvider } from "./service/contexts/useAuth";
 
 export default function App() {
+  const WrappedContactBook = (
+    <ContactBookProvider>
+      <NewContactModalProvider>
+        <GroupsContextProvider>
+          <GroupsModalContextProvider>
+            <Header />
+            <ContactsPlace />
+            <NewContactModal />
+            <GroupsModal />
+            <FavoritesButton />
+            <PersistContactBook />
+            <PersistGroups />
+          </GroupsModalContextProvider>
+        </GroupsContextProvider>
+      </NewContactModalProvider>
+    </ContactBookProvider>
+  );
+
+  const AuthToContactBook = (
+    <AuthDataProvider>
+      <AuthPage />
+    </AuthDataProvider>
+  );
+
+  const { user } = useUser();
+
   return (
     <div className="App">
-      <ContactBookProvider>
-        <NewContactModalProvider>
-          <GroupsContextProvider>
-            <GroupsModalContextProvider>
-              <Header />
-              <ContactsPlace />
-              <NewContactModal />
-              <GroupsModal />
-              <FavoritesButton />
-              <PersistContactBook />
-              <PersistGroups />
-            </GroupsModalContextProvider>
-          </GroupsContextProvider>
-        </NewContactModalProvider>
-      </ContactBookProvider>
+      <BrowserRouter>
+        <Switch>
+          {user && <Redirect from="/authorization" to={"/"} />}
+          <AuthRoute path="/" exact={true} children={WrappedContactBook} />
+          <Route path="/authorization" children={AuthToContactBook} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
